c61d9397e7099f11baf0e4025ca5d02b3d255e86
Mona Mohamed <111650146+MonaMohamedAbdelaziz2003@users.noreply.github.com>
2023-09-13 23:27:17 +0300

    Add files via upload

---
+++ composit pattern/arithmetic.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class arithmetic(ABC):
+    @abstractmethod
+    def getvalue(self):
+        pass
---
+++ composit pattern/client.py
@@ -0,0 +1,4 @@
+from operation import operation
+from Numeric import numeric
+arithmetic1=operation(numeric(4),numeric(5),'+')
+print(arithmetic1.getvalue())
